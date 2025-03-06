3058afafadab563a459be711e1600928eb1893c6
Bhavesh Mahajan <bhmahajan055@gmail.com>
2025-03-07 00:11:58 +0530

    Initial Commit

---
+++ ECommerceApplication/src/main/java/com/app/payloads/UserDTO.java
@@ -0,0 +1,24 @@
+package com.app.payloads;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.app.entites.Role;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+	
+	private Long userId;
+	private String firstName;
+	private String lastName;
+	private String mobileNumber;
+	private String email;
+	private String password;
+	private Set<Role> roles = new HashSet<>();
+	private AddressDTO address;
+	private CartDTO cart;
+}
